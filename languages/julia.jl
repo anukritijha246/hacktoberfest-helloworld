25a70e2437f3805a51c4936ebdb60f1c690dc140
Amit Dutta <amitdutta2406@gmail.com>
2019-10-03 18:43:08 +0530

    Added Hello World in julia

---
+++ languages/julia.jl
@@ -0,0 +1,6 @@
+// To execute this program open terminal or command prompt
+// Switch to the folder where julia.jl is saved
+// Run the program by typing: julia julia.jl
+// Make sure to add PATH to environment variables in advanced system properties before executing script
+
+println("Hello World")
